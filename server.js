bcf4ccda2fb4c3d91737ca22f805c89d3293f42e
dkgus12dkgus <dkgs12dkgus@naver.com>
2022-03-14 20:29:18 +0900

    FEAT: initial init

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("API running"));
+// Connect to mongodb
+// const URI = process.env.MONGODB_URL;
+// mongoose.connect(
+//   URI,
+//   {
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+//   (err) => {
+//     if (err) throw err;
+//     console.log("Connected to mongodb");
+//   }
+// );
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
