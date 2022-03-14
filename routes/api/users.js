5e25a19ba99d1c91c5f41858a8d3fb66b359d7f5
dkgus12dkgus <dkgs12dkgus@naver.com>
2022-03-14 23:47:13 +0900

    ADD: 토큰발급

--- routes/api/users.js
+++ routes/api/users.js
@@ -56,8 +56,22 @@ router.post(
       const salt = await bcrypt.genSalt(10);
 
       user.password = await bcrypt.hash(password, salt);
-
       await user.save();
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send("Server error");
