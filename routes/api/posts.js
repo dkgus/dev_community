93fa6d9f2f2d3861ab6ed9a42e5115833148690a
dkgus12dkgus <dkgs12dkgus@naver.com>
2022-03-14 23:17:53 +0900

    ADD: 회원가입 기능추가

---
+++ routes/api/posts.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+/**
+ *  @route  GET api/post
+ *  @desc   Test route
+ *  @access public
+ */
+
+router.get("/", (req, res) => res.send("Post Router"));
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,17 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./config/db");
+
+connectDB();
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => res.send("API running"));
-// Connect to mongodb
-// const URI = process.env.MONGODB_URL;
-// mongoose.connect(
-//   URI,
-//   {
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   },
-//   (err) => {
-//     if (err) throw err;
-//     console.log("Connected to mongodb");
-//   }
-// );
+
+/** router */
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/posts", require("./routes/api/posts"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
