bd9c3baf33b57093558865658113851e4e31cd80
dkgus12dkgus <dkgs12dkgus@naver.com>
2022-03-21 23:34:45 +0900

    ADD:프로필 추가 및 수정 API추가

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth"); //사용자의 id를 가져오기위해 auth필요
+const { check, validationResult } = require("express-validator");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
@@ -31,5 +32,89 @@ router.get("/me", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+/**
+ *  @route  POST api/profile
+ *  @desc   Create or update user profile
+ *  @access private
+ */
+
+router.post(
+  "/",
+  [
+    auth,
+    [
+      check("status", "직업을 적어주세요").not().isEmpty(),
+      check("skills", "스킬을 적어주세요").not().isEmpty(),
+    ],
+  ],
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    //build profile object
+    const profileFileds = {};
+    profileFileds.user = req.user.id;
+    if (company) profileFileds.company = company;
+    if (website) profileFileds.website = website;
+    if (location) profileFileds.location = location;
+    if (bio) profileFileds.bio = bio;
+    if (status) profileFileds.status = status;
+    if (githubusername) profileFileds.githubusername = githubusername;
+    if (skills) {
+      profileFileds.skills = skills.split(",").map((skill) => skill.trim());
+    }
+    console.log(skills);
+    console.log(profileFileds.skills);
+    //res.send("HELLO");
+
+    //build social object
+    profileFileds.social = {};
+    if (youtube) profileFileds.social.youtube = youtube;
+    if (twitter) profileFileds.social.twitter = twitter;
+    if (facebook) profileFileds.social.facebook = facebook;
+    if (linkedin) profileFileds.social.linkedin = linkedin;
+    if (instagram) profileFileds.social.instagram = instagram;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+      //update
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileFileds },
+          { new: true }
+        );
+        return res.json(profile);
+      }
+
+      // Create
+      profile = new Profile(profileFileds);
+      await profile.save();
+      return res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
