23a4614b22ad9a04c93895db2227ad9eda50ee81
dkgus12dkgus <dkgs12dkgus@naver.com>
2022-03-23 22:37:32 +0900

    ADD: 경험항목 추가

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -161,7 +161,7 @@ router.get("/user/:user_id", async (req, res) => {
 /**
  *  @route  DELETE api/profile
  *  @desc   Delete profile & user & posts
- *  @access Private
+ *  @access private
  */
 
 router.delete("/", auth, async (req, res) => {
@@ -180,4 +180,52 @@ router.delete("/", auth, async (req, res) => {
     res.status(500).send("Server Error");
   }
 });
+
+/**
+ *  @route  PATCH api/profile/experience
+ *  @desc   add profile experience
+ *  @access private
+ */
+router.put(
+  "/experience",
+  [
+    auth,
+    [
+      check("title", "제목은 필수입니다").not().isEmpty(),
+      check("company", "회사를 기입해주세요").not().isEmpty(),
+      check("from", "입사일을 기입해주세요").not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { title, company, location, from, to, current, description } =
+      req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json("Server Error");
+    }
+  }
+);
+
 module.exports = router;
