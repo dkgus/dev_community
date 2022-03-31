469e4551ee7d79fe2f8b6d0ed290000880f42cb7
dkgus12dkgus <dkgs12dkgus@naver.com>
2022-03-31 23:03:08 +0900

    ADD: 기본 퍼블리싱 추가 및 리액트 라우터 적용

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+  return <div style={{ paddingTop: 100 }}>Login</div>;
+};
+
+export default Login;
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Register = () => {
+  return <div style={{ paddingTop: 100 }}>Register</div>;
+};
+
+export default Register;
