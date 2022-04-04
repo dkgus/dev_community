b5a3ba0bbbaf521b58eaa12f141efd68e35be47c
dkgus <dkgs12dkgus@naver.com>
2022-04-04 23:02:09 +0900

    ADD: alert redux system 추가

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
-const Register = () => {
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -16,9 +19,9 @@ const Register = () => {
   const onSubmit = (e) => {
     e.preventDefault();
     if (password !== password2) {
-      console.log("비밀번호가 일치하지 않습니다");
+      setAlert("비밀번호가 일치하지 않습니다", "danger");
     } else {
-      console.log("SUCCESS");
+      setAlert("회원가입 되었습니다");
     }
   };
 
@@ -89,4 +92,7 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+export default connect(null, { setAlert })(Register);
---
+++ client/src/components/layout/Alert.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+const Alert = ({ alerts }) => (
+  <div className="alert-wrapper">
+    {alerts.map((alert) => (
+      <div
+        key={alert.id}
+        className={`alert alert-${alert.alertType}`}
+        style={{ marginTop: 70 }}
+      >
+        {alert.msg}
+      </div>
+    ))}
+  </div>
+);
+
+Alert.propTypes = {
+  alerts: PropTypes.array.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  alerts: state.alert,
+});
+
+export default connect(mapStateToProps)(Alert);
