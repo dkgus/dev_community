0536fac1983ef33bc09052c9f9a1a02e2b670ac4
dkgus <dkgs12dkgus@naver.com>
2022-04-14 23:21:44 +0900

    ADD: profile display

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,9 @@ import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
 import AddExperience from "./components/profile-forms/AddExperience";
-import PrivateRoute from "./components/routing/PrivateRouting";
 import AddEducation from "./components/profile-forms/AddEducation";
+import Profiles from "./components/profiles/Profiles";
+import PrivateRoute from "./components/routing/PrivateRouting";
 
 //redux
 import { Provider } from "react-redux";
@@ -37,6 +38,8 @@ const App = () => {
             <Route path="/" element={<Landing />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
+            <Route path="/profiles" element={<Profiles />} />
+
             <Route
               path="dashboard"
               element={<PrivateRoute component={Dashboard} />}
